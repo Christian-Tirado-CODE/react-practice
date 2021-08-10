58bd59c210d584a965e81e8a9dc951ee914dd6eb
Christian <tiradoc02@gmail.com>
2021-08-09 21:10:42 -0400

    changed variable name

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -2,7 +2,7 @@ import classes from './CartItem.module.css';
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store/cart-slice';
 const CartItem = (props) => {
-  const {id, name, quantity, total, price } = props.item;
+  const {id, title, quantity, total, price } = props.item;
   const dispatch = useDispatch();
 
   const increaseQtyHandler = () => {
@@ -22,7 +22,7 @@ const CartItem = (props) => {
   return (
     <li className={classes.item}>
       <header>
-        <h3>{name}</h3>
+        <h3>{title}</h3>
         <div className={classes.price}>
           ${total.toFixed(2)}{' '}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
