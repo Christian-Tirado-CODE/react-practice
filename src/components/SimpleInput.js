c51e08152168516f2fbea1f202c366930eb3982c
Christian <tiradoc02@gmail.com>
2021-08-11 09:54:06 -0400

    Val. on keystroke, refactoring and Deriving state.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
   /*
    1. USER INPUT SHOULD BE VALIDATED ON THE SERVER FOR SECURITY REASONS 
    BECAUSE USERS CAN MANIPULATE CODE ON THE BROWSER.
+
+   2. When to validate:
+       a. when form is submitted
+       b. When an input is losing focus
+       c. On every keystroke
   
   */
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const nameInputRef = useRef();
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
+
 
   useEffect(() => {
     if(enteredNameIsValid){
@@ -19,30 +25,42 @@ const SimpleInput = (props) => {
   }, [enteredNameIsValid]);
 
   const nameInputChangedHandler = (event) => {
+
     setEnteredName(event.target.value);
-    
+    if(!enteredNameIsValid){
+        return;
+    }
+
   };
+
+
+  const nameInputBlurHandler = () => {
+
+    setEnteredNameTouched(true);
+
+    if(!enteredNameIsValid){
+      
+      return;
+    }
+
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if(enteredName.trim() === ''){
-      setEnteredNameIsValid(false);
+    if(!enteredNameIsValid){
       return;
     }
 
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
 
     setEnteredName('');
-   // enteredValue.current.value = ''; NOT IDEAL, DO NOT MANIPULATE THE DOM.
-  
+    setEnteredNameTouched(false);
   };
 
   
-  const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
 
@@ -50,7 +68,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={nameInputRef} value={enteredName} onChange={nameInputChangedHandler}/>
+        <input type='text' id='name' value={enteredName} onChange={nameInputChangedHandler} onBlur={nameInputBlurHandler}/>
         { nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
