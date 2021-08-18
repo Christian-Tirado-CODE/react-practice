81e0b7040240b46ff24f78aa87103c289e6aa119
Christian <tiradoc02@gmail.com>
2021-08-18 14:13:04 -0400

    Managing State

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useRef } from 'react';
 
+
 const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
