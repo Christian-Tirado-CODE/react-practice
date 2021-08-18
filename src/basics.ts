f5de847a4bbffa5c86d4e5df44044e913681560e
Christian <tiradoc02@gmail.com>
2021-08-17 21:38:56 -0400

    Basics: primitives and reference types.

---
+++ src/basics.ts
@@ -0,0 +1,46 @@
+//Primitives: 
+// Arrays and Objects:
+// Function Types, parameters
+
+
+
+// Primitives
+
+let age: number = 24;
+age = 12;
+
+let username: string;
+username = 'Christian';
+
+let isChristianHandsome:boolean;
+isChristianHandsome = true;
+
+
+// Reference Types
+
+//Arrays
+let names: string[];
+names = ['Shawn', 'Tyrone', 'Brad', 'Juano'];
+
+
+// Objects
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Juano',
+    age: 32
+}
+
+let people: {
+ name: string;
+ age: number;
+}[];
+
+people = [
+    {name: 'Shakira', age: 28},
+    {name: 'Joe', age: 80}
+]
+
