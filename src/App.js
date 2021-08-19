ac0e3c9c1c230338342561805a01f2da6f000659
Christian <tiradoc02@gmail.com>
2021-08-19 09:59:35 -0400

    Side effects explanation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /*
+    What is a side effect?
+    Everything else outside of React's main job which is to render UI and React to User Input.
+    Examples: sending http requests to backend servers, timers, storing data in browser storage.
+
+    Side effects are managed with the useEffect hook.
+    Has two parameters: 1. A function that runs AFTER every component evaluation IF the dependencies changed.
+                        2. Dependencies of this effect. The function only runs if the dependencies changed.
+  */
+
   return (
     <div className="App">
       <header className="App-header">
