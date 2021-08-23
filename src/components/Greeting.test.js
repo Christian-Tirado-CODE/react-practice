d0e996fac4226a695729cf570259c49cfc4197b2
Christian <tiradoc02@gmail.com>
2021-08-22 22:54:59 -0400

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 describe('Greeting Component', () => {
     test('Renders hello world as a test', () => {
@@ -24,6 +25,41 @@ describe('Greeting Component', () => {
         
         
         }); 
+
+ // When testing you want to check ALL possible scenarios.
+
+
+ test("renders 'It's good to see you!' if button is NOT clicked", () => {
+     const component = render(<Greeting />);
+     const paragraphElement =  screen.getByText("It's good to see you!");
+     expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test("renders 'Changed!' if the button was clicked", () => {
+        const component = render(<Greeting />);
+        
+        const buttonElement = screen.getByRole('button');
+
+        userEvent.click(buttonElement);
+
+        const paragraphElement =  screen.getByText("Changed!");
+
+
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test("does not render 'Good to see you!' if button is not clicked", () => {
+        const component = render(<Greeting />);
+        
+        const buttonElement = screen.getByRole('button');
+
+        userEvent.click(buttonElement);
+
+        const paragraphElement =  screen.queryByText("It's good to see you!");
+
+        expect(paragraphElement).toBeNull();
+        
+    })
 })
 
 
