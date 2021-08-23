f830e66e5180d9e388c1492137f797fa67ff4895
Christian <tiradoc02@gmail.com>
2021-08-22 21:13:02 -0400

    Kinds of Tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ /*
+    Types of Tests: Unit tests, integration tests and End-to-End tests
+    Unit tests= testing all the individual building blocks(functions, components) in isolation.
+    Integration tests= Test the combination of building blocks. A project typically contains a couple of integration tests.
+    End-to-end tests= Test complete scenarios in your app just as the user would experience them. A project contains only a few e-to-e tests.
+ 
+ */
+
+
   return (
     <div className="App">
       <header className="App-header">
