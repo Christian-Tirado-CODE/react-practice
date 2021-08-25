59756835d777ab9751f3d156f517b804cdd104e7
Christian <tiradoc02@gmail.com>
2021-08-25 14:31:09 -0400

    How React really works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  /*
+    React - It's a library that only cares and knows about components, state, props, context and finds out what components might need to change based on the differences of the previous and current state of a component.
+    It does not know about html elements. It does not know about the browser. It sends the information regarding what changed and what should be visible on the screen to the ReactDOM.
+    React uses the Virtual DOM to determine how the component tree currently looks like and what it should look like. That info is handed to ReactDOM
+    ReactDOM - Interface to the web. Responsible for managing real html elements and bringing something on to the screen. It receives the differences in the Virtual DOM and updates the real DOM.
+
+    Re-evaluating components !== Re-rendering the DOM
+    Components - re-evaluated when the props, state or context changes. React executes component functions.
+
+
+    Real DOM - Changes to the real DOM are only made for differences in evaluations. Making changes to the real DOM is performance intesive so we want to update only parts of the UI that need to be changed.
+  */
   return (
     <div className="App">
       <header className="App-header">
