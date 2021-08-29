1dc507de14058df691cd595346349a8facf32601
Christian <tiradoc02@gmail.com>
2021-08-29 15:12:17 -0400

    Handling http errors

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  /* Data is typically sent in JSON format which is easy to convert to javascript objects. The response object has the json() method that does that.*/
+  const [error, setError] = useState(null);
+  /* 
+  Data is typically sent in JSON format which is easy to convert to javascript objects. The response object has the json() method that does that.
+   Status codes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+  */
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
+    setError(null);
+     try { 
+
       const response = await fetch('https://swapi.dev/api/films');
+      if(!response.ok){
+        throw new Error(response.status);
+     }
+
       const data = await response.json();
+
+     
  
       const transformedMovies = data.results.map(movie => {
           return {
@@ -20,8 +33,11 @@ function App() {
           }
       });
       setMovies(transformedMovies);
-      setIsLoading(false);
-
+      
+    }catch(error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   };
   
 
@@ -32,8 +48,10 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading &&  <p>Loading...</p>}
+
       </section>
     </React.Fragment>
   );
