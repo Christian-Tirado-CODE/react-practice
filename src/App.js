2ca6e25ccb2278590dad57a12dc7ea8a2a09d848
Christian <tiradoc02@gmail.com>
2021-08-29 14:42:24 -0400

    Async and await

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   /* Data is typically sent in JSON format which is easy to convert to javascript objects. The response object has the json() method that does that.*/
-  const fetchMoviesHandler = () => {
-      fetch('https://swapi.dev/api/films').then(response => {
-          return response.json();
-      }).then(data => {
-        const transformedMovies = data.results.map(movie => {
+  const fetchMoviesHandler = async () => {
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
+ 
+      const transformedMovies = data.results.map(movie => {
           return {
             id: movie.episode_id,
             title: movie.title,
             openingText: movie.opening_crawl,
             releaseDate: movie.release_date
           }
-        });
-         setMovies(transformedMovies);
       });
+      setMovies(transformedMovies);
 
   };
   
