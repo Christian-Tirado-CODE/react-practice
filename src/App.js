089f522835911c85375215afb308cb2a91262d7c
Christian <tiradoc02@gmail.com>
2021-08-29 20:08:48 -0400

    Defining and using Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Route} from "react-router-dom";
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
 function App() {
+  /* 
+    React router handles different paths and loads components based on those given paths.
+    http://ourdomain/ => Component A
+    http://ourdomain/products => Component B 
+
+    The components handled by react-router can be considered special. It is common to put these components on a separate folder called pages or screens.
+    
+  */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/welcome">
+      <Welcome />
+      </Route>
+      <Route path="/products">
+      <Products/>
+      </Route>
     </div>
   );
 }
