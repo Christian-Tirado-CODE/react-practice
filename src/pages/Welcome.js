b379ff945fbaeb0ddce426328a47aba89062ad7a
Christian <tiradoc02@gmail.com>
2021-09-01 06:56:10 -0400

    Working with Nested Routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,13 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import {Route} from "react-router-dom";
 const Welcome = () => {
     return (
+        <Fragment>
         <h1>The Welcome page!</h1>
+        <Route path="/welcome/new-user">
+         <p>Welcome, new user!</p>
+        </Route>
+        </Fragment>
     );
 };
 
