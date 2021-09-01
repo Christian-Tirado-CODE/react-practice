339b7f8819a16b418c4b7ac4a4709adc68795abe
Christian <tiradoc02@gmail.com>
2021-09-01 06:50:59 -0400

    Using "Switch" and "exact" For Configuring Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
 import ProductDetails from './pages/ProductDetails';
+
 function App() {
   /* 
     React router handles different paths and loads components based on those given paths.
@@ -15,16 +16,24 @@ function App() {
   */
   return (
     <div className="App">
+      
       <MainHeader />
+      <Switch>
       <Route path="/welcome">
       <Welcome />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>   {/*Both routes are active because they begin with the same path. 
+                                 That's how react router works. 
+                                 If you don't want this behaviour use the Switch component. 
+                                 now the product details does not appear on the screen because react finds the first match which is /products. 
+                                 We can fix this by using the exact prop.
+                                 */}
       <Products/>
       </Route>
-      <Route path="/product-details/:productId">
+      <Route path="/products/:productId">
       <ProductDetails/>
       </Route>
+      </Switch>
     </div>
   );
 }
